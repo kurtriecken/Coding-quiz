9da40e5417727c9ecc2e8908d3076886b3b0bc65
Kurt Riecken <kurt.riecken@gmail.com>
2023-09-29 09:42:08 -0400

    added remove functionality; testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+let welcome = document.createElement("h1");
+welcome.innerHTML = `Welcome to the coding quiz!`;
+
+const center = document.getElementById("center_card");
+
+center.insertBefore(welcome, center.children[0]);
+
+const button = document.getElementById("start");
+
+button.addEventListener("click", removeFunction);
+
+function removeFunction() {
+    let child = center.children[0];
+    child.remove();
+}
+
 // const head = document.getElementById("head");
 // let scores = document.createElement("a");
 
