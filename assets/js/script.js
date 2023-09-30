c681bfd609b69c6adccc097ddca07fa917c1f071
Kurt Riecken <kurt.riecken@gmail.com>
2023-09-30 11:35:17 -0400

    added mouseover to hide prev answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,11 +10,13 @@ const clearScoresButton = document.getElementById("clear_scores");
 const HSButton = document.getElementById("view_scores");
 
 // Sections to hide/unhide
+const timer = document.getElementById("timer");
 const welcome = document.getElementById("welcome_card");
 const qCard = document.getElementById("question_card");
 const questionText = document.getElementById("question_text");
 const choices = document.getElementById("choice_wrapper");
 const initialsEntry = document.getElementById("initials_entry");
+const scoreText = document.getElementById("score_text");
 const qResult = document.getElementById("question_result");
 const highScores = document.getElementById("high_scores");
 
@@ -41,6 +43,9 @@ let quizQuestions =
   ];
 
 // Other variables
+const timerWord = "Time: "
+let timeRemaining = 0;
+let timeVar;
 let currQuestion = "";
 let questSolution = "";
 let choiceArr;
@@ -50,10 +55,17 @@ let questArr;
 // Was the answer right? Print result to bottom of screen
 
 function init() {
+  timeRemaining = 75;
   currQuestion = "";
   questSolution = "";
   choiceArr = [];
   questArr = structuredClone(quizQuestions);
+  timer.innerHTML = timerWord + timeRemaining;
+}
+
+function tickDown() {
+  timeRemaining--;
+  timer.innerHTML = timerWord + timeRemaining;
 }
 
 function generateNewQuestion(button) {  
@@ -66,10 +78,14 @@ function generateNewQuestion(button) {
     }
     else {
       qResult.innerHTML = "Wrong =(";
+      // TODO: Timer minus 10
+      timeRemaining -= 10;
     }
   }
 
   if (questArr.length == 0) {
+    clearInterval(timeVar);
+    scoreText.innerHTML += timeRemaining;
     qCard.setAttribute("hidden", "hidden");
     initialsEntry.removeAttribute("hidden");
     return;
@@ -142,6 +158,7 @@ function changeDisplay(e) {
 
 // Event listeners
 startButton.addEventListener("click", function(event) {
+  timeVar = setInterval(tickDown, 1000);
   changeDisplay(event)
 });
 
@@ -154,5 +171,15 @@ choices.addEventListener("click", function(event) {
   // console.log(event.target);
   changeDisplay(event)
 });
+
+choices.addEventListener("mouseover", function(e) {
+  e.stopPropagation();
+  console.log("I see you...");
+  let mouseOver = e.target.nodeName;
+  if (mouseOver !== 'BUTTON') {
+    return;
+  }
+  qResult.setAttribute("hidden", "hidden");
+})
 
 init();
